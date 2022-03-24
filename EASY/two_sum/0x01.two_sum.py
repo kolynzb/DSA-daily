1096e4e761aab175603de0abf1e0e4bdc517e7ca
kolynzb <collinsbenda360@gmail.com>
2022-03-24 10:31:07 +0300

    Two sum algo

---
+++ EASY/two_sum/0x01.two_sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(nums):
+            
