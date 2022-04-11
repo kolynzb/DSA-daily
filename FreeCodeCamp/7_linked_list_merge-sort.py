3a174e64e7d2b3dfba26aae8769002bddec848bd
kolynzb <collinsbenda360@gmail.com>
2022-04-11 09:55:33 +0300

    add nothing

--- FreeCodeCamp/7_linked_list_merge-sort.py
+++ FreeCodeCamp/7_linked_list_merge-sort.py
@@ -1,11 +1,16 @@
-from 5_linked_list import LinkedList
+from ./5_linked_list import LinkedList
 
 def merge_sort(linked_list):
     """ 
     Sorts a linked ist in ascending order 
     - Recusrsively divide the linked list into sublists containing a single node
     - Repeatly merge the  sublist to produce sorted lists until one remains until one remains 
-
+    Returns a sorted linked list 
     """
+    if linked_list.size() == 1:
+        return linked_list
+    elif linked_list.head is None:
+        return linked_list
 
-    3:39:37
+    lef_half,right_half 3:40:21
+
