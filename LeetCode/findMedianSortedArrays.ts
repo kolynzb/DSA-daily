a8c90a02c57ec7313302580ad9c391c9b6d95a10
kolynzb <collinsbenda360@gmail.com>
2022-11-05 22:47:43 +0300

    Add Solution to median of sorted array

--- LeetCode/findMedianSortedArrays.ts
+++ LeetCode/findMedianSortedArrays.ts
@@ -1,12 +1,14 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    //   Join array and sort it
-    const newArr = nums1.concat(nums2).sort();
-    console.log(newArr.length/2)
-    if (newArr.length % 2 == 0) {
-      return (newArr[(newArr.length / 2) - 1] + newArr[(newArr.length / 2 + 1)-1]) / 2;
-    } else {
-      return newArr[(((newArr.length - 1) / 2 ) +1) -1];
-    }
+  //   Join array and sort it
+  const newArr = nums1.concat(nums2).sort((a, b) => a - b);
+  console.log(newArr);
+  if (newArr.length % 2 == 0) {
+    return (
+      (newArr[newArr.length / 2 - 1] + newArr[newArr.length / 2 + 1 - 1]) / 2
+    );
+  } else {
+    return newArr[(newArr.length - 1) / 2 + 1 - 1];
   }
-  
-  console.log(findMedianSortedArrays([1, 2], [4]));
+}
+
+console.log(findMedianSortedArrays([3], [-2, -1]));
