79e63e3be6bc2d9124d240d0398ad4789432f72f
kolynzb <collinsbenda360@gmail.com>
2023-10-27 10:58:03 +0300

    10/27/2023

--- colt-steele/patterns/is-subsequence.ts
+++ colt-steele/patterns/is-subsequence.ts
@@ -17,12 +17,25 @@ Time Complexity - O(N + M)
 Space Complexity - O(1)
 */
 
-function isSubsequence(str: string, subStr: string): boolean {
-    
-  return true;
+function isSubsequence(subStr: string, str: string): boolean {
+  if (str.length < subStr.length) return false;
+  let i = 0;
+  let j = 0;
+
+  while (j < str.length) {
+    if (subStr[i] === str[j]) {
+      i++;
+    }
+    j++;
+    if (i === subStr.length - 1) {
+      return true;
+    }
+  }
+  return false;
 }
 
-isSubsequence("hello", "hello world"); // true
-isSubsequence("sing", "sting"); // true
-isSubsequence("abc", "abracadabra"); // true
-isSubsequence("abc", "acb"); // false (order matters)
+console.log(isSubsequence("hello", "hello world"));
+console.log(isSubsequence("sing", "sting"));
+console.log(isSubsequence("abc", "abracadabra"));
+console.log(isSubsequence("abc", "acb"));
+// TODO: Review This
